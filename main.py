2dff2608beb82b420dd65e0bf63d7a0bf1ae97ae
K-chris <kooyman.christopher@gmail.com>
2022-06-06 05:49:54 -0700

    added classes for input and move

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def move_player():
 
 def get_item():
     return -1
+
+
+def user_input(commands):
+    while True:
+        user_choice=input('what would you like to do: ').upper()
+        seperated_input=user_choice.split()
+        if seperated_input[0] in commands:
+            print(seperated_input)
+            return seperated_input
+
+
 def print_player_info(name,inventory):
     print("{}".format(name))
     print("Inventory :[]".format(inventory))
@@ -18,6 +29,7 @@ if __name__ == '__main__':
     player_name=None
     current_room='Hanger Bay'
     inventory=[]
+    commands=['GET','USE','MOVE','GO']
     items={
         'Cargo Bay': 'Corrupted Data Drive',
         'Hanger Bay': 'NONE',
@@ -50,15 +62,22 @@ if __name__ == '__main__':
     print("collect items and unlock the way to the boss to win")
     print("Move commands: GO NORTH,GO SOUTH, GO EAST GO WEST")
     print("Add item to Inventory: GET 'Item Name'")
-    print("USE item from Inventory: USE 'Item Name'\n\n")
+    print("USE item from Inventory: USE 'Item Name'\n")
     x=1
 
     while player_name is None:
-        player_name=input('Enter in your player name\n')
+        player_name=input('Enter in your player name: ')
         print('\n\n')
 
     while True:
         print_player_info(player_name,inventory)
         print_room_info(current_room,items)
-        User_move=user_input()
+        print('_________________________________________')
+
+        user_choice=user_input(commands)
+        if user_choice[0]=='MOVE' or 'go'
+            move_player
+        #elif user_choice[0] ==
+        #elif user_choice[0] ==
+
         break
