b0c5da53b6d4e9d37143e56ac70958092c1dd8d3
K-chris <kooyman.christopher@gmail.com>
2022-06-06 07:02:23 -0700

    Minor adjustments to spacings and syntax

--- main.py
+++ main.py
@@ -1,61 +1,68 @@
+def move_player(usr_choice, cur_room):  # function move player when user_choice contains MOVE or GO
+    # checks to see what direction was inputted and compares it with room list to see if it's a valid move
+    if usr_choice[1] in rooms.get(cur_room):
 
+        return rooms[cur_room].get(usr_choice[1])  # updates room to new room
+    else:  # function prints that it's an invalid move and returns the same room back
+        print("There is no door that way")
+        return cur_room
 
-def move_player():
-    return -1
 
 def get_item():
     return -1
 
 
-def user_input(commands):
+def user_input():
     while True:
-        user_choice=input('what would you like to do: ').upper()
-        seperated_input=user_choice.split()
-        if seperated_input[0] in commands:
-            print(seperated_input)
-            return seperated_input
+        usr_choice = input('what would you like to do: ').upper()
+        separated_input = usr_choice.split()
+        if separated_input[0] in commands:
+            print(separated_input)
+            return separated_input
 
 
-def print_player_info(name,inventory):
+def print_player_info(name):
     print("{}".format(name))
-    print("Inventory :[]".format(inventory))
+    print("Inventory :{}".format(inventory))
 
-def print_room_info(current_room,items):
-    print("you are currently in the: {}".format(current_room))
-    if(items[current_room]!='NONE'):
-        print('You see a {} on the ground'.format(items[current_room]))
+
+def print_room_info(cur_room):
+    print("you are currently in the: {}".format(cur_room))
+    if items[cur_room] != 'NONE':
+        print('You see a {} on the ground'.format(items[cur_room]))
+
 
 if __name__ == '__main__':
-    player_name=None
-    current_room='Hanger Bay'
-    inventory=[]
-    commands=['GET','USE','MOVE','GO']
-    items={
+    player_name = 'CHAD'  # TODO CHANGE TO None when done testing
+    current_room = 'Hanger Bay'
+    inventory = []
+    commands = ['GET', 'USE', 'MOVE', 'GO']
+    items = {  # make a Dictionary of all rooms and what item might be in there
         'Cargo Bay': 'Corrupted Data Drive',
         'Hanger Bay': 'NONE',
         'Lower Engines': 'Fire Extinguisher',
-        'Generator Room': 'Portable SHield Generator',
+        'Generator Room': 'Portable SHeld Generator',
         'Upper Engines': 'Wrench',
         'Medical bay': 'First Aid Kit',
         'Crew Quarters': 'Phaser',
-        'Hallway':'Medbay Key card',
+        'Hallway': 'Med-bay Key card',
         'Mess Hall': 'Mystery Meat Paste',
         'Kitchen': 'Kitchen Knife',
         'Cockpit': 'NONE'
     }
 
-    rooms={
-        'Cargo Bay': {'NORTH':'Crew Quarters', 'WEST': 'Hanger Bay'},
-        'Hanger Bay': {'EAST':'Cargo Bay','WEST':'Lower Engine'},
-        'Lower Engines': {'NORTH':'Generator Room',"EAST":'Hanger Bay'},
-        'Generator Room': {'North':'Upper Engines','SOUTH':'Lower Engines'},
-        'Upper Engines': {'EAST':'Medical bay','SOUTH':'Generator Room'},
-        'Medical bay': {'EAST': 'Crew Quarters','WEST':'Upper Engines'},
-        'Crew Quarters':{'EAST':'Hallway','SOUTH':"Cargo Bay"},
-        'Hallway':{'SOUTH':'Mess Hall', 'WEST': 'Crew Quarters'},
-        'Mess Hall':{'NORTH':'Hallway', 'EAST':'Cockpit','SOUTH':'Kitchen'},
-        'Kitchen':{'North':'Mess Hall'},
-        'Cockpit':{'WEST':'Mess Hall'}
+    rooms = {  # set Dictionary room to list of all rooms and the valid adjacent rooms
+        'Cargo Bay': {'NORTH': 'Crew Quarters', 'WEST': 'Hanger Bay'},
+        'Hanger Bay': {'EAST': 'Cargo Bay', 'WEST': 'Lower Engines'},
+        'Lower Engines': {'NORTH': 'Generator Room', "EAST": 'Hanger Bay'},
+        'Generator Room': {'NORTH': 'Upper Engines', 'SOUTH': 'Lower Engines'},
+        'Upper Engines': {'EAST': 'Medical bay', 'SOUTH': 'Generator Room'},
+        'Medical bay': {'EAST': 'Crew Quarters', 'WEST': 'Upper Engines'},
+        'Crew Quarters': {'EAST': 'Hallway', 'SOUTH': "Cargo Bay", 'WEST': 'Medical bay'},
+        'Hallway': {'SOUTH': 'Mess Hall', 'WEST': 'Crew Quarters'},
+        'Mess Hall': {'NORTH': 'Hallway', 'EAST': 'Cockpit', 'SOUTH': 'Kitchen'},
+        'Kitchen': {'NORTH': 'Mess Hall'},
+        'Cockpit': {'WEST': 'Mess Hall'}
     }
 
     print("Untitled:A Space Game")
@@ -63,21 +70,18 @@ if __name__ == '__main__':
     print("Move commands: GO NORTH,GO SOUTH, GO EAST GO WEST")
     print("Add item to Inventory: GET 'Item Name'")
     print("USE item from Inventory: USE 'Item Name'\n")
-    x=1
 
     while player_name is None:
-        player_name=input('Enter in your player name: ')
+        player_name = input('Enter in your player name: ')
         print('\n\n')
 
     while True:
-        print_player_info(player_name,inventory)
-        print_room_info(current_room,items)
+        print_player_info(player_name)
+        print_room_info(current_room)
         print('_________________________________________')
 
-        user_choice=user_input(commands)
-        if user_choice[0]=='MOVE' or 'go'
-            move_player
-        #elif user_choice[0] ==
-        #elif user_choice[0] ==
-
-        break
+        user_choice = user_input()
+        if user_choice[0] == 'MOVE' or 'GO':
+            current_room = move_player(user_choice, current_room)
+        # elif user_choice[0] ==
+        # elif user_choice[0] ==
