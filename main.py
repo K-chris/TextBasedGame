98f9aa3f4c8ae7cb2f2dde6f149de2767db5cd29
K-chris <kooyman.christopher@gmail.com>
2022-06-13 16:25:16 -0700

    reoptimized and almost ready for submission

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import socket
+# christopher Kooyman
+# IT-140-H7429
+# 22EW5
+# 6/13/2022
+
+
+def player_instructions():  # funct to print the game commands and objective
+    print("Shipwrecked:A Space Game",
+          "collect all 9 items and regain control of the ship, or die trying\n",
+          "Move commands: GO NORTH,MOVE SOUTH, GO EAST, MOVE WEST\n",
+          "Add item to Inventory: GET 'Item Name'\n",
+          "Exit the game: EXIT\n\n")
 
 
 def move_player(usr_choice, cur_room):  # function move player when user_choice contains MOVE or GO
@@ -12,10 +23,11 @@ def move_player(usr_choice, cur_room):  # function move player when user_choice 
 
 
 def get_item(usr_choice, cur_room):  # function for getting items and putting into inventory
-    if usr_choice[1] in items.get(cur_room).upper() and usr_choice[1] != 'NONE':  #checks to see if user input is the same item in the current room
-        inventory.append(items[cur_room])  #add item to inventory
-        print("You obtained a {}".format(items[cur_room]))  #let user know they obtained a item
-        items[cur_room] = 'NONE'  #set the item in the room to none so the user cannot pick it up again
+    if usr_choice[1] in items.get(cur_room).upper() and usr_choice[1] != 'NONE':  # checks to see if user input is
+        # the same item in the current room
+        inventory.append(items[cur_room])  # add item to inventory
+        print("You obtained a {}".format(items[cur_room]))  # let user know they obtained an item
+        items[cur_room] = 'NONE'  # set the item in the room to none so the user cannot pick it up again
     else:
         print("Unable to obtain {}".format(usr_choice[1]))
 
@@ -30,59 +42,59 @@ def user_input():  # function user_input for processing user input and checkin i
             return separated_input  # returns the user input list of size 2
 
 
-def print_player_info(name):  # function for printing player info
+def print_player_info(name):  # function for printing player info   #TODO CONSOLIDATE  INTO SH OW STATUS FUNCTION
     print("{}".format(name))  # Print users name
     print("Inventory :{}".format(inventory))  # print users current inventory
 
 
-def print_room_info(cur_room):
+def print_room_info(cur_room):   #TODO CONSOLIDATE INTO SHOW STATUS FUNCTION
     print("you are currently in the: {}".format(cur_room))
     if items[cur_room] != 'NONE':
         print('You see a {} on the ground'.format(items[cur_room]))
 
 
-def main():
-    player_name = socket.gethostname()  # TODO CHANGE TO None when done testing
-    current_room = 'Lower Engines'  # TODO CHANGE TO Hanger Bay when deploying game
-    print("Untitled:A Space Game")
-    print("collect items and unlock the way to the boss to win")
-    print("Move commands: GO NORTH,GO SOUTH, GO EAST GO WEST")
-    print("Add item to Inventory: GET 'Item Name'")
-    print("USE item from Inventory: USE 'Item Name'\n")
+def final_boss():
+    print("You enter in the Cockpit ready for what horrors might be inside.")
+    if len(inventory) == 9:
 
+        print("the space pirate was unaware of your presence as you entered and you strike \n",
+              "while he is unaware, taking him out with a single blast of your Phaser\n",
+              'GAME OVER, YOU WIN!! Thanks for Playing')
+    else:
+        print("The space pirate was expecting you and had a ambush set up as you entered,/n",
+              'blasting you to pieces.\n', 'GAME OVER, YOU LOSE. Thanks for Playing')
+
+
+def main():
+    player_name = None
+    current_room = 'Lower Engines'  # TODO CHANGE TO Hanger Bay when deploying game
+    player_instructions()
     while player_name is None:
         player_name = input('Enter in your player name: ')
         print('\n\n')
 
-    while True:
+    while current_room != 'Cockpit':
         print_player_info(player_name)
         print_room_info(current_room)
         print('_________________________________________')
-
         user_choice = user_input()
         if user_choice[0] in ['GO', 'MOVE']:
             current_room = move_player(user_choice, current_room)
         elif user_choice[0] in 'GET':
             get_item(user_choice, current_room)
-        # elif user_choice[0] ==
+        elif user_choice[0] in 'EXIT':
+            print("You decide to exit the abandoned ship, leaving behind the mystery of what had happened")
+            print("Game Over, hope you had fun")
+            return
+
+    if current_room in 'Cockpit':
+        final_boss()
 
 
 if __name__ == '__main__':
     inventory = []
-    commands = ['GET', 'USE', 'MOVE', 'GO']
-    door_locks = {  # DICTIONARY OF ROOM LOCK STATE
-        'Cargo Bay': 'UNLOCKED',
-        'Hanger Bay': 'UNLOCKED',
-        'Lower Engines': 'UNLOCKED',
-        'Generator Room': 'UNLOCKED',
-        'Upper Engines': 'UNLOCKED',
-        'Medical bay': 'LOCKED',
-        'Crew Quarters': 'LOCKED',
-        'Hallway': 'UNLOCKED',
-        'Mess Hall': 'UNLOCKED',
-        'Kitchen': 'UNLOCKED',
-        'Cockpit': 'LOCKED'
-    }
+    commands = ['GET', 'MOVE', 'GO']
+
     items = {  # make a Dictionary of all rooms and what item might be in there
         'Cargo Bay': 'Corrupted Data Drive',
         'Hanger Bay': 'NONE',
